dd31053171df6635d6cc6cff67fac0618f8eca47
Joshua Johnson <jj17@iu.edu>
2020-07-10 19:54:46 -0400

    class hiearchy

--- src/main/java/com/codedifferently/casino/Deck.java
+++ src/main/java/com/codedifferently/casino/Deck.java
@@ -5,13 +5,21 @@ import java.util.Collections;
 
 public class Deck {
     ArrayList<Card> cards = new ArrayList();//holds all the cards in the deck
-    //constructs the deck with 13 cards of each suite
-    Deck() {
-        for (Card.Suite suite : Card.Suite.values()) {
-            for (Card.Rank rank : Card.Rank.values()) {
+    ArrayList<Card> usedCards = new ArrayList<>();
+
+
+    Deck()
+    {
+        //constructs the deck with 13 cards of each suite
+        for (Card.Suite suite : Card.Suite.values())
+        {
+            for (Card.Rank rank : Card.Rank.values())
+            {
                 cards.add(new Card(suite, rank));
             }
         }
+
+
     }
 
     public Card deal() {
--- src/main/java/com/codedifferently/casino/Game.java
+++ src/main/java/com/codedifferently/casino/Game.java
@@ -2,26 +2,42 @@ package com.codedifferently.casino;
 
 public interface Game {
     public void startGame();
+    //end game method
 }
 
-public class BlackJack extends CardGame implements Game{
-    public void startGame(){
+public class BlackJack extends CardGame {
+    public void startGame() {
         this.dealCards();
-        this.startTurn();   }
- public class goFish implements Game {
-     public void startGame() {
-     }
- }
-     public class blackJack implements Game{
-            public void startGame(); {}}
-            public class MyApp{
-            public void selectGame(String selection){
-                BlackJack blackjack = new BlackJack();
-                goFish hilo = new goFish();
-                this.startGame(blackJack);
-                this.startGame(goFish);
+        this.startTurn();
     }
-        public void startGame(Game selectedGame){
+
+    public class goFish implements Game
+    {
+        public void startGame()
+        {
+
+        }
+    }
+
+    public class blackJack implements Game {
+
+
+        public void startGame()
+        {
+        }
+    }
+
+    public class MyApp {
+        public void selectGame(String selection) {
+            BlackJack blackjack = new BlackJack();
+            goFish hilo = new goFish();
+            this.startGame(blackJack);
+            this.startGame(goFish);
+        }
+
+        public void startGame(Game selectedGame) {
             selectedGame.startGame();
 
-        }}}
+        }
+    }
+}
