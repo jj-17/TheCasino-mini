26cdeedb6b3932800673c9c61623053c80ca51b6
Joshua Johnson <jj17@iu.edu>
2020-07-12 10:19:35 -0400

    starting Blackjack

--- src/main/java/com/codedifferently/casino/BlackJack.java
+++ src/main/java/com/codedifferently/casino/BlackJack.java
@@ -1,4 +1,23 @@
 package com.codedifferently.casino;
 
 public class BlackJack extends CardGame {
+
+    Player player;
+    Deck card_Deck;
+
+    public BlackJack(Player player)
+    {
+        this.player = player;
+        this.card_Deck = new Deck();
+    }
+
+    public void startGame()
+    {
+
+    }
+
+    public void endGame()
+    {
+
+    }
 }
