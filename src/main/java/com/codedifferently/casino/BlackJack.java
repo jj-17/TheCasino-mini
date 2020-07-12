6937b6c6467c85ec3784de326a5653ca299df8e0
Joshua Johnson <jj17@iu.edu>
2020-07-12 12:22:52 -0400

    should have picked gofish

--- src/main/java/com/codedifferently/casino/BlackJack.java
+++ src/main/java/com/codedifferently/casino/BlackJack.java
@@ -24,25 +24,42 @@ public class BlackJack extends CardGame {
 
     public void setPlayerMoney(double money) {
         double playersCurrentMoney = this.getPlayer().getMoney();
-        boolean completedBet = false;
 
-        while (!completedBet)
-        {
-            if (money <= this.getPlayer().getMoney()) {
-                this.getPlayer().setMoney(playersCurrentMoney - money);
-                completedBet = true;
-            } else {
-                System.out.println("Your attempted bet exceeds your money.");
+        this.getPlayer().setMoney(playersCurrentMoney - money);
+    }
+
+    public boolean checkBet() {
+        Scanner sc = new Scanner(System.in);
+
+        double playersCurrentMoney = this.getPlayer().getMoney();
+        boolean validBet = false;
+        System.out.println("Please enter your initial bet: ");
+        double attemptedBet = sc.nextDouble();
+
+        while (!validBet) {
+
+            if (attemptedBet <= playersCurrentMoney)
+            {
+
+                this.setPlayerMoney(attemptedBet);
+                System.out.println("You have made a bet of $"+attemptedBet);
+                System.out.println("Your new balance is "+ this.getPlayer().getMoney());
+                validBet = true;
+            }
+            else {
+                System.out.println("You do not have enough money to bet $"+attemptedBet);
+                System.out.println("Please enter a bet");
             }
         }
+
     }
 
     public void startGame()
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your initial bet: ");
-        double playersBet = sc.nextDouble();
-        //this.setPlayerMoney();
+
+
+
+
 
     }
 
---
+++ src/test/java/com/codedifferently/casino/BlackJackTest.java
@@ -0,0 +1,28 @@
+package com.codedifferently.casino;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlackJackTest {
+
+    @Test
+    public void getPlayer() {
+    }
+
+    @Test
+    public void setPlayerMoney() {
+    }
+
+    @Test
+    public void checkBet() {
+    }
+
+    @Test
+    public void startGame() {
+    }
+
+    @Test
+    public void endGame() {
+    }
+}
